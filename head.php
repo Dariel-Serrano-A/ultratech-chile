a904463f80945a2d499bdd53aba1504838ef982e
Dariel Serrano <dariel.serrano@gmail.com>
2024-03-28 02:16:19 -0300

    updt head

--- head.php
+++ head.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>UltraTech - El poder de la tecnología a tu alcance.</title>
+<title>UltraTech - Concepcion - Lomas Coloradas - Desarrollo Web - Servicio Tecnico - Soporte Informatico - 2024 </title>
+<meta name="description" content="Servicios profesionales de desarrollo web, soporte técnico, hosting, software licenciado y marketing digital. Soluciones personalizadas para impulsar tu presencia en línea.">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 <link rel="icon" href="imagenes/base-icon.png" type="image/png">
--- vars.php
+++ vars.php
@@ -1,7 +1,7 @@
 <?php 
-    $l_inicio = '"http://ultratech.infinityfreeapp.com//inicio.php"';
-    $l_precios = '"http://ultratech.infinityfreeapp.com//precios.php"';
-    $l_prfs = '"http://ultratech.infinityfreeapp.com//prfs.php"';
+    $l_inicio = '"http://ultratech.infinityfreeapp.com/inicio.php"';
+    $l_precios = '"http://ultratech.infinityfreeapp.com/precios.php"';
+    $l_prfs = '"http://ultratech.infinityfreeapp.com/prfs.php"';
     $l_email = '"mailto:ultratech.chile@gmail.com?subject=Contacto%20desde%20sitio%20web&body=Escriba%20sus%20dudas,%20consultas%20o%20comentarios%20aqui..."';
     $l_wsp = '"https://wa.me/56953077891"';
     $l_ig = '"https://www.instagram.com/ultratech.chile"';
