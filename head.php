b880f7efa015766c13e7572bc47b887b1d1df17a
Dariel Serrano <dariel.serrano@gmail.com>
2024-04-30 11:20:20 -0400

    updte meta

--- head.php
+++ head.php
@@ -21,4 +21,4 @@
 <!-- CSS, JS y otros metadatos -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
-<link rel="icon" href="imagenes/base-icon.png" type="image/png">
+<link rel="icon" href="imagenes/meta-icon.png" type="image/png">
