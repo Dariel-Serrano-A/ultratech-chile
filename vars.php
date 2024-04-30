608f02a47f17499de531b0e03bad84568c0115be
Dariel Serrano <dariel.serrano@gmail.com>
2024-04-30 01:20:08 -0400

    updt estilos

--- vars.php
+++ vars.php
@@ -1,7 +1,7 @@
 <?php 
-    $l_inicio = '"http://localhost/ultratech/inicio.php"';
-    $l_precios = '"http://localhost/ultratech/precios.php"';
-    $l_prfs = '"http://localhost/ultratech/prfs.php"';
+    $l_inicio = '"https://ultratech.infinityfreeapp.com/inicio.php"';
+    $l_precios = '"https://ultratech.infinityfreeapp.com/precios.php"';
+    $l_prfs = '"https://ultratech.infinityfreeapp.com/prfs.php"';
     $l_email = '"mailto:ultratech.chile@gmail.com?subject=Contacto%20desde%20sitio%20web&body=Escriba%20sus%20dudas,%20consultas%20o%20comentarios%20aqui..."';
     $l_wsp = '"https://wa.me/56953077891"';
     $l_ig = '"https://www.instagram.com/ultratech.chile"';
