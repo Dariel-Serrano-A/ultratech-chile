ebbe5b57addd18ace3036f99bf1efc8c24c91c0e
Dariel Serrano <dariel.serrano@gmail.com>
2024-05-02 16:33:29 -0400

    updte format .php

--- preciostabla.php
+++ preciostabla.php
@@ -26,24 +26,29 @@ try {
             }
         }
         
-        // Si el array de celdas no está vacío, imprime la fila
-        if (!empty($cells)) {
-            // Determina si la celda en la segunda columna contiene el texto "Precio"
-            $secondColumnValue = isset($cells[1]) ? $cells[1] : null;
-            $isPriceRow = $secondColumnValue === "Precio";
+    // Si el array de celdas no está vacío, imprime la fila
+    if (!empty($cells)) {
+        // Determina si la celda en la segunda columna contiene el texto "Precio"
+        $secondColumnValue = isset($cells[1]) ? $cells[1] : null;
+        $isPriceRow = $secondColumnValue === "Precio";
 
-            // Imprime la fila con las celdas marcadas como <th> si es una fila de "Precio"
-            echo "<tr>";
-            foreach ($cells as $key => $value) {
-                $cellTag = $isPriceRow ? "th" : "td";
-                if ($key == 1 && is_numeric($value)) {
-                    echo "<$cellTag class=\"centrar-celda\">" . $value . "</$cellTag>";
-                } else {
-                    echo "<$cellTag>" . $value . "</$cellTag>";
-                }
+        // Imprime la fila con las celdas marcadas como <th> si es una fila de "Precio" y centra con la clase añadida si es numero
+        echo "<tr>";
+        foreach ($cells as $key => $value) {
+            $cellTag = $isPriceRow ? "th" : "td";
+            if ($key == 1 && is_numeric($value)) {
+                // Formatea el número para que cada tres dígitos tenga un punto como separador de miles
+                $formattedValue = number_format($value, 0, ',', '.');
+                echo "<$cellTag class=\"centrar-celda\">$" . $formattedValue . "CLP</$cellTag>";
+            } else if ($value === "Gratis" || $value === "Precio") {
+                echo "<$cellTag class=\"centrar-celda\">" . $value . "</$cellTag>";
+            } else {
+                echo "<$cellTag>" . htmlspecialchars($value) . "</$cellTag>";
             }
-            echo "</tr>";
         }
+        echo "</tr>";
+    }
+
     }
 
     echo "</table>";
