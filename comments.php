8b5753b3213f6d7e4da1814fdbfe737cff6e59d9
Dariel Serrano <dariel.serrano@gmail.com>
2024-08-12 14:32:56 -0400

    order changes

--- comments.php
+++ comments.php
@@ -1,29 +1,29 @@
 
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <?php include 'head.php' ;?>          
-</head>
-<body>
-    <header>  
-    </header>
-    
-    <nav>
-        <?php include 'nav.php' ;?>
-    </nav>
+ <head>
+  <?php include 'head.php' ;?>          
+ </head>
+ <body>
+  <header>  
+  </header>
+  
+  <nav>
+   <?php include 'nav.php' ;?>
+  </nav>
 
-    <main>
-        <div class="container-fluid px-5 cntr-base">
-            <div class="row">             
-                <div class="col-xl-12 mb-3 px-2 center">
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeW7agrM-f3pfq8uNlIIM1f7ZegsF41PvIH0Poz-1N2hf4Dig/viewform?embedded=true" width="800" height="1000" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
-                </div>                
-            </div>
-        </div>
-    </main>
+  <main>
+   <div class="container-fluid px-5 cntr-base">
+    <div class="row">             
+     <div class="col-xl-12 mb-3 px-2 center">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeW7agrM-f3pfq8uNlIIM1f7ZegsF41PvIH0Poz-1N2hf4Dig/viewform?embedded=true" width="800" height="1000" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+     </div>                
+    </div>
+   </div>
+  </main>
 
-    <footer>
-        <?php include 'footer.php' ;?>
-    </footer>
-</body>
+  <footer>
+   <?php include 'footer.php' ;?>
+  </footer>
+ </body>
 </html>
